e07af87ba8e61c217042a8ebd3e27d219c47a565
Aya Alsaudi <ayaalsaudi6@gmail.com>
2026-06-08 02:44:31 +0300

    Fix path issues for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dashboard-React/'
-})
-export default defineConfig({
-  plugins: [react()],
   base: '/Dashboard-React/', 
 })
-
